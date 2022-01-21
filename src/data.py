8ed0e81055f9bb0f02c042675a7fb6d70840e8a5
spalmurray <spalmurray@gmail.com>
2022-01-21 17:07:39 -0500

    Added scoreboard commands! The bot can now do everything I intended for it to do!

--- src/data.py
+++ src/data.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import Tuple, List
 from pymongo.mongo_client import MongoClient
 
 
--- src/main.py
+++ src/main.py
@@ -27,8 +27,17 @@ async def on_message(message):
         stats = database.get_player_stats(message.author.id)
         player = message.author.nick if message.author.nick is not None else message.author.name
         stats_string = f"{player}'s average number of guesses is {stats[0]}. They've played {stats[1]} games and won " \
-                       f"{stats[2]} games, making their win rate {stats[3] * 100}%."
+                       f"{stats[2]} games, making their win rate {round(stats[3] * 100, 2)}%."
         await message.channel.send(stats_string)
+
+    if message.content == "!wb average":
+        await message.channel.send(rankings_by_average(message, 10))
+
+    if message.content == "!wb rate":
+        await message.channel.send(rankings_by_win_rate(message, 10))
+
+    if message.content == "!wb games":
+        await message.channel.send(rankings_by_games_played(message, 10))
 
     if message.content == "!wb helper":
         await message.channel.send('https://www.spalmurray.com/wordle-helper')
@@ -36,6 +45,9 @@ async def on_message(message):
     if message.content == "!wb help" or message.content == "!wb":
         help_string = "`!wb help` to see this message\n" \
                       "`!wb me` to see your stats\n" \
+                      "`!wb average` to see server rankings by average number of guesses\n" \
+                      "`!wb rate` to see server rankings by win rate\n" \
+                      "`!wb games` to see server rankings by games played\n" \
                       "`!wb helper` for a link to wordle-helper"
         await message.channel.send(help_string)
 
@@ -68,6 +80,75 @@ async def on_message(message):
             await message.channel.send("Cutting it a little close there...")
         else:
             await message.channel.send("I will pretend like I didn't see that one...")
+
+
+def rankings_by_average(message, n: int) -> str:
+    """Return string formatted leaderboard ordered by average guesses where message is the message data from the
+    triggering Discord message and n is the max number of rankings to return.
+    """
+    members = [(member.nick if member.nick is not None else member.name, member.id)
+               for member in message.guild.members]
+    scores = []
+    for member in members:
+        score = database.get_player_stats(member[1])
+        if score[0] == 0:
+            continue
+        scores.append((member[0], score))
+    scores.sort(key=lambda x: x[1][0])
+
+    scoreboard = "Rankings by average number of guesses:"
+    i = 0
+    while i < n and i != len(scores):
+        scoreboard += f"\n{i + 1}. {scores[i][0]} ({scores[i][1][0]})"
+        i += 1
+
+    return scoreboard
+
+
+def rankings_by_win_rate(message, n: int) -> str:
+    """Return string formatted leaderboard ordered by win rate where message is the message data from the
+    triggering Discord message and n is the max number of rankings to return.
+    """
+    members = [(member.nick if member.nick is not None else member.name, member.id)
+               for member in message.guild.members]
+    scores = []
+    for member in members:
+        score = database.get_player_stats(member[1])
+        if score[0] == 0:
+            continue
+        scores.append((member[0], score))
+    scores.sort(key=lambda x: x[1][3], reverse=True)
+
+    scoreboard = "Rankings by win rate:"
+    i = 0
+    while i < n and i != len(scores):
+        scoreboard += f"\n{i + 1}. {scores[i][0]} ({round(scores[i][1][3] * 100, 2)}%)"
+        i += 1
+
+    return scoreboard
+
+
+def rankings_by_games_played(message, n: int) -> str:
+    """Return string formatted leaderboard ordered by number of games played where message is the message data from the
+    triggering Discord message and n is the max number of rankings to return.
+    """
+    members = [(member.nick if member.nick is not None else member.name, member.id)
+               for member in message.guild.members]
+    scores = []
+    for member in members:
+        score = database.get_player_stats(member[1])
+        if score[0] == 0:
+            continue
+        scores.append((member[0], score))
+    scores.sort(key=lambda x: x[1][1], reverse=True)
+
+    scoreboard = "Rankings by games played:"
+    i = 0
+    while i < n and i != len(scores):
+        scoreboard += f"\n{i + 1}. {scores[i][0]} ({scores[i][1][1]})"
+        i += 1
+
+    return scoreboard
 
 
 if __name__ == "__main__":
